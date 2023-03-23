ef4db219d3aa9b50930e9d9bd01d07878e115219
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-03-23 15:04:49 +0100

    added-user-input

--- src/org/lessons/java/Nations.java
+++ src/org/lessons/java/Nations.java
@@ -7,7 +7,11 @@
 
 package org.lessons.java;
 
+import com.mysql.cj.util.PerVmServerConfigCacheFactory;
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+
 import java.sql.*;
+import java.util.Scanner;
 
 public class Nations {
 
@@ -17,6 +21,11 @@ public class Nations {
         String USER = "root";
         String PASSWORD = "root";
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Search regions:");
+        String userString = input.nextLine();
+
         try(Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
 
             String query = """
@@ -25,11 +34,15 @@ public class Nations {
                         from regions r
                         join continents c
                         on c.continent_id = r.continent_id
+                        where r.name like "%"?"%"
                         order by r.name asc;
                         
                     """;
 
             try(PreparedStatement ps = conn.prepareStatement(query)){
+
+                ps.setString(1, String.valueOf(userString));
+
                 try(ResultSet rs = ps.executeQuery()) {
                     while (rs.next()){
                         String name = rs.getString("r.name");
